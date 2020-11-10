64a6bb1667a90bb9ca520f60e86f8fb84b6a3ced
Ruben Matamoros <ruben@valiantcreative.net>
2020-11-09 23:26:45 -0500

    added ability to update and delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 var pageContentEl = document.querySelector("#page-content");
+var tasksInProgessEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 var taskIdCounter = 0;
 
 var taskFormHandler = function(event) {
@@ -145,7 +147,29 @@ var editTask = function(taskId) {
 
     formEl.removeAttribute("data-task-id");
     document.querySelector("#save-task").textContent = "Add Task";
+  }
+
+  var taskStatusChangeHandler = function(event) {
+      // get the task item's id
+      var taskId = event.target.getAttribute("data-task-id");
+
+      // get the currently selected option's value and covnert to lowercase
+      var statusValue = event.target.value.toLowerCase();
+
+      // find the parent task item element based on the id
+      var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+        if (statusValue === "to do") {
+          tasksToDoEl.appendChild(taskSelected);
+        }
+        else if (statusValue === "in progress") {
+          tasksInProgessEl.appendChild(taskSelected);
+        }
+        else if (statusValue === "completed") {
+          tasksCompletedEl.appendChild(taskSelected);
+        }
   };
 
       formEl.addEventListener("submit", taskFormHandler);
       pageContentEl.addEventListener("click", taskButtonHandler);
+      pageContentEl.addEventListener("change", taskStatusChangeHandler);
